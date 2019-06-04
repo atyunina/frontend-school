d1ce4866ecfd55da7a18baae7a2350061d71ce3f
atyunina <tyunina.ai@gmail.com>
2019-06-04 21:37:10 +0400

    Graph class first draft

---
+++ entry-testing-master/3. algorithm/Graph.js
@@ -0,0 +1,64 @@
+class Edge {
+    constructor(src, dest, weight) {
+        this.src = src;
+        this.dest = dest,
+        this.weight = weight;
+    }
+}
+
+class Graph { 
+	constructor(noOfVertexes, directed) { 
+        this.noOfVertices = noOfVertexes;
+        this.directed = directed;
+		this.AdjacencyList = new Map(); 
+    }
+    
+    addVertex(v) { 
+        this.AdjacencyList.set(v, []); 
+    }
+
+    addEdge(src, dest, weight) { 
+        let edge = new Edge(src, dest, weight);
+        this.AdjacencyList.get(src).push(edge);  
+        if (!this.directed) {
+            let reversed = new Edge(dest, src, weight);
+            this.AdjacencyList.get(dest).push(reversed);
+        }
+    }
+
+    printGraph() 
+    { 
+        let keys = this.AdjacencyList.keys(); 
+  
+        for (let i of keys)  
+        { 
+            let values = this.AdjacencyList.get(i); 
+            let conc = ""; 
+  
+            for (var j of values) 
+                conc += j.dest + " "; 
+  
+            console.log(i + " -> " + conc); 
+        } 
+    } 
+}
+
+function main() {
+    var g = new Graph(6, false); 
+    let vertices = [ 'A', 'B', 'C', 'D', 'E', 'F' ];
+    for (var i = 0; i < vertices.length; i++) { 
+        g.addVertex(vertices[i]); 
+    }
+    g.addEdge('A', 'B', 3); 
+    g.addEdge('A', 'D', 2); 
+    g.addEdge('A', 'E', 1); 
+    g.addEdge('B', 'C', 4); 
+    g.addEdge('D', 'E', 7); 
+    g.addEdge('E', 'F', 0); 
+    g.addEdge('E', 'C', 5); 
+    g.addEdge('C', 'F', 6); 
+
+    g.printGraph();
+}
+
+alert(main());
